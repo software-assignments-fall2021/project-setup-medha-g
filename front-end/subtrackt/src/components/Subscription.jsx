45aa321ec55ffbd7dcd4917c5f7b2c2b7bd92d66
Medha Gupta <medha4@10-21-27-20.dynapool.wireless.nyu.edu>
2021-10-31 23:25:49 -0400

    fixed grid for subscription list

--- front-end/subtrackt/src/components/Subscription.jsx
+++ front-end/subtrackt/src/components/Subscription.jsx
@@ -10,19 +10,19 @@ function Subscription(props) {
 
 	return (
 		<div className="row">
-			<div className="column col-xs-6 col-md-4">
+			<div className="column col-xs-6 col-md-3">
 				<h4>{props.title}</h4>
 				<img src={props.image} alt="Logo"></img>
 			</div>
-			<div className="column col-xs-6 col-md-4">
+			<div className="column col-xs-6 col-md-33">
 				<h4>Description</h4>
 				<p>{props.description}</p>
 			</div>
-			<div className="column col-xs-6 col-md-4">
+			<div className="column col-xs-6 col-md-3">
 				<h4>Plan Details</h4>
 				<p>Price: {props.plan.price} for {props.plan.time_quantity} {props.plan.time_unit}</p>
 			</div>
-			<div className="column col-xs-6 col-md-4">
+			<div className="column col-xs-6 col-md-3">
 				<button type="button" onClick={handleClick}>Delete</button>
 			</div>
 		</div>
