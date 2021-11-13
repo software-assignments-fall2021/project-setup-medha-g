0bda591ade95b0b1c6a22f5e8f85aa79548c1aee
Morgan Xu <hx801@nyu.edu>
2021-11-13 17:30:23 -0500

    added plaid support

--- back-end/routes/api/index.js
+++ back-end/routes/api/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.use("/users", require("./users"));
 router.use("/test", require("./test"));
+router.use("/plaid", require("./plaid"));
 
 module.exports = router;
