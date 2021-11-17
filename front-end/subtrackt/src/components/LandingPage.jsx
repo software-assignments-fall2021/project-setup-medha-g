ccd883d03820f0d99fb4f2de0c9a8a4744dfcfb1
Medha Gupta <medha4@10-21-128-137.dynapool.wireless.nyu.edu>
2021-11-17 18:28:06 -0500

    added css to landing page

--- front-end/subtrackt/src/components/LandingPage.jsx
+++ front-end/subtrackt/src/components/LandingPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import graphGIF from '../images/graph_animation_nobg.gif';
 
 const LandingPage = () => {
     return (
         <div className="landing">
             <div>
-                SubTrackt is a subscription tracking and budgeting application.
+                <header id="showcase">
+                    <h1>Welcome to SubTrackt</h1>
+                        <p>SubTrackt is a subscription tracking and budgeting application.</p>
+                        <a href="#section-a" class="button">Read More</a>
+                </header>
+                <section id="section-a">
+                <h2>Understand Where Your Money is Going!</h2>
+                <img src={graphGIF} alt="Graph Animation" />
+                    <div class="box1">
+                        View all of your subscriptions via your credit card transaction history, cancel any unwanted subscriptions, and keep track of your spending with SubTrackt.
+                    </div>
+                </section>
             </div>
         </div>
     )
