8e1c9663ca224f1da79e2c51ccc2191b7e9e66f8
Medha Gupta <medha4@10-22-10-29.dynapool.wireless.nyu.edu>
2021-11-18 17:32:52 -0500

    added css to login

--- front-end/subtrackt/src/components/Header.jsx
+++ front-end/subtrackt/src/components/Header.jsx
@@ -5,9 +5,9 @@ import LogoutPage from './LogOutPage';
 const Header = () => {
     return (
         <div className="navbar">
-            <Link className="navbar-nav" to="/landing">
+            <Link className="column" to="/landing">
             <img alt="Qries" src="../favicon.ico" width="15%"></img></Link>
-            <Link className="navbar-nav" to="/sub_list">Log In/Sign Up</Link>
+            <Link className="column login-text" to="/sub_list">Log In/Sign Up</Link>
             <LogoutPage />
         </div>
     )
