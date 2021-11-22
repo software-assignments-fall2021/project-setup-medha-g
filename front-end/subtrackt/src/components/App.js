c9fdc52670856eb9e737db2d647b7ee9ea4ad5de
Medha Gupta <medha4@MacBook-Air-2.fios-router.home>
2021-11-21 20:19:31 -0500

    ffixed app.js to be protected page

--- front-end/subtrackt/src/components/App.js
+++ front-end/subtrackt/src/components/App.js
@@ -1,45 +1,7 @@
-// import React from 'react'
-// import "../styles/App.css";
-// import LandingPage from "./LandingPage";
-// import Header from "./Header";
-// import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-// import { ProvideAuth } from "./use-auth";
-// import ProtectPage from './ProtectPage'
-// import { Redirect } from 'react-router';
-
-
-// function App() {
-//   return (
-//     <ProvideAuth>
-//       <div className="App">
-//         <Router>
-//           <Header />
-//           <Switch>
-//             <Route path="/landing">
-//               <LandingPage />
-//             </Route>
-
-//             <Route path="/sub_list">
-//               <ProtectPage />
-//             </Route>
-
-//             <Route path="/">
-//                   <Redirect to="/landing"></Redirect>
-//             </Route>
-//           </Switch>
-//         </Router>
-//       </div>
-//     </ProvideAuth>
-//   );
-// }
-
-// export default App;
-
 import React from 'react'
 import "../styles/App.css";
 import LandingPage from "./LandingPage";
 import Header from "./Header";
-import SubscriptionList from './SubscriptionList';
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { ProvideAuth } from "./use-auth";
 import ProtectPage from './ProtectPage'
@@ -58,8 +20,7 @@ function App() {
             </Route>
 
             <Route path="/sub_list">
-              <SubscriptionList />
-
+              <ProtectPage />
             </Route>
 
             <Route path="/">
@@ -73,3 +34,4 @@ function App() {
 }
 
 export default App;
+
