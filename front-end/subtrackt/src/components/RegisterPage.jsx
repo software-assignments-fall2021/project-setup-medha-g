fc294832e37dd1b43ef575e29cf4c8aa7faf025c
Medha Gupta <medha4@MacBook-Air-2.fios-router.home>
2021-11-27 21:09:19 -0500

    added footer component

--- front-end/subtrackt/src/components/RegisterPage.jsx
+++ front-end/subtrackt/src/components/RegisterPage.jsx
@@ -2,11 +2,7 @@ import React, { useState } from "react";
 import { useAuth } from "./use-auth";
 import { useHistory, useLocation } from "react-router-dom";
 import useToggle from "./use-toggle";
-
-import twitter from '../images/twitter-icon.png';
-import facebook from '../images/facebook-icon.png';
-import linkedin from '../images/linkedin-icon.png';
-import instagram from '../images/instagram-icon.png';
+import Footer from "./Footer.jsx";
 
 
 const RegisterPage = (props) => {
@@ -61,57 +57,7 @@ const RegisterPage = (props) => {
                 <button type="submit" className="custom-button" onClick={handleSubmit}>Submit</button>
                 <br/><br/><p className="signup_login_link">Already have an account? <a href="#" onClick={props.handleChange}>Sign in</a></p><br/><br/>
             </form>
-            <div className="footer-dark footer-login">
-                <footer>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-sm-6 col-md-3 item">
-                                <h3>Services</h3>
-                                <ul>
-                                    <li><a href="#">Development</a></li>
-                                    <li><a href="#">Resources</a></li>
-                                    <li><a href="#">FAQ</a></li>
-                                </ul>
-                            </div>
-                            <div className="col-sm-6 col-md-3 item">
-                                <h3>About</h3>
-                                <ul>
-                                    <li><a href="#">Company</a></li>
-                                    <li><a href="#">Team</a></li>
-                                    <li><a href="#">Careers</a></li>
-                                </ul>
-                            </div>
-                            <div className="col-md-6 item text">
-                                <h3>SubTrackt</h3>
-                                <p>SubTrackt is a Web Application that allows users to keep track of their subscriptions. Subscriptions are either populated via credit card transaction history or via manual entering. Users also have the opportunity to cancel and add subscriptions that they are interested in through SubTrackt and they can evaluate exactly where each of their money is going.</p>
-                            </div>
-                            <div className="col item social">
-                                <a href="#">
-                                    <i className="icon ion-social-facebook">
-                                        <img src={facebook} height="20px" alt="" />
-                                    </i>
-                                </a>
-                                <a href="#">
-                                    <i className="icon ion-social-twitter">
-                                        <img src={twitter} height="20px" alt="" />
-                                    </i>
-                                </a>
-                                <a href="#">
-                                    <i className="icon ion-social-snapchat">
-                                        <img src={linkedin} height="20px" alt="" />
-                                    </i>
-                                </a>
-                                <a href="#">
-                                    <i className="icon ion-social-instagram">
-                                        <img src={instagram} height="20px" alt="" />
-                                    </i>
-                                </a>
-                            </div>
-                        </div>
-                        <p className="copyright">SubTrackt © 2021</p>
-                    </div>
-                </footer>
-            </div>
+            <Footer/>
             {auth.errMessage ? <div>{auth.errMessage}</div> : null}
         </div>
     );
