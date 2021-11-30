49a6e4110b4d4b2fc001b99d031e89c6afefe8be
Morgan Xu <hx801@nyu.edu>
2021-11-29 23:22:56 -0500

    Added user unit tests

--- back-end/test/index.js
+++ back-end/test/index.js
@@ -3,12 +3,11 @@ const request = require('supertest')
 const chai = require('chai')
 require('dotenv').config({ silent: true })
 
-
 describe('Server', function () {
     let date, server
     const app = require('../app')
 
-    this.timeout(5000);
+    this.timeout(5000)
 
     before(function (done) {
         server = app.listen(3000, function (err) {
@@ -19,23 +18,13 @@ describe('Server', function () {
         })
     })
 
-    beforeEach(function () {
-        date = new Date()
-    })
-
     after(function () {
         server.close()
-        console.log('All tests done')
-    })
-
-    afterEach(function () {
-        console.log('Tested on ', date)
     })
 
     it('should be up', function (done) {
         request(app)
             .get('/api/test/checkup')
-            .expect('Content-Type', /json/)
             .expect(200, function (err, res) {
                 if (err) return done(err)
 
@@ -43,5 +32,60 @@ describe('Server', function () {
                 chai.expect(message).to.equal(true)
                 done()
             })
+    })
+
+    describe('User Module', function () {
+        it('should recognize admin', function (done) {
+            const user_admin = process.env.ADMIN_USER
+            const user_pass = process.env.ADMIN_PASS
+
+            request(app)
+                .post('/api/users/login')
+                .send({ user: { username: user_admin, password: user_pass } })
+                .expect(200, function (err, res) {
+                    request(app)
+                        .get('/api/users/current')
+                        .set('Authorization', `Token ${res.body.user.token}`)
+                        .expect(200, function (err, res) {
+                            if (err) done(err)
+
+                            chai.expect(res.body.user.username).to.equal(
+                                'admin'
+                            )
+                            done()
+                        })
+                })
+        })
+
+        it('should recognize invalid jwt', function (done) {
+            request(app)
+                .get('/api/users/getsublist')
+                .set('Authorization', `Token badToken`)
+                .expect(401, function (err, res) {
+                    done()
+                })
+        })
+
+        it('should recognize valid jwt', function (done) {
+            const user_admin = process.env.ADMIN_USER
+            const user_pass = process.env.ADMIN_PASS
+
+            request(app)
+                .post('/api/users/login')
+                .send({ user: { username: user_admin, password: user_pass } })
+                .expect(200, function (err, res) {
+                    request(app)
+                        .get('/api/test/checkAuthorize')
+                        .set('Token', res.body.user.token)
+                        .expect(200, function (err, res) {
+                            if (err) done(err)
+
+                            chai.expect(res.body.message).to.equal(
+                                'If you see this mesasage you are authorized.'
+                            )
+                            done()
+                        })
+                })
+        })
     })
 })
