52da24f869b372bc1890285bf598bfb7fcebc73c
Zachary <zw1490@nyu.edu>
2021-11-30 20:24:22 -0500

    added unit tests for register and delete account

--- back-end/app.js
+++ back-end/app.js
@@ -1,49 +1,51 @@
-const express = require("express");
-const path = require("path");
-const session = require("express-session");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const errorHandler = require("errorhandler");
-require("dotenv").config({ silent: true });
+const express = require('express')
+const path = require('path')
+const session = require('express-session')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const errorHandler = require('errorhandler')
+require('dotenv').config({ silent: true })
 
-mongoose.promise = global.Promise;
+mongoose.promise = global.Promise
 
-const isProduction = process.env.NODE_ENV === "production";
-const DB_ADDR = process.env.DB_ADDR;
-const SESSION_SECRET = process.env.SESSION_SECRET;
+const isProduction = process.env.NODE_ENV === 'production'
+const DB_ADDR = process.env.DB_ADDR
+const SESSION_SECRET = process.env.SESSION_SECRET
 
-const app = express();
+const app = express()
 
 // configure the app
-app.use(cors());
-app.use(require("morgan")("dev"));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(cors())
+// app.use(require('morgan')('dev'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(
-	session({
-		secret: SESSION_SECRET,
-		cookie: { maxAge: 60000 },
-		resave: false,
-		saveUninitialized: false,
-	})
-);
+    session({
+        secret: SESSION_SECRET,
+        cookie: { maxAge: 60000 },
+        resave: false,
+        saveUninitialized: false,
+    })
+)
 
-if (!isProduction) app.use(errorHandler());
-
+if (!isProduction) {
+    app.use(require('morgan')('dev'))
+    app.use(errorHandler())
+}
 app.use((err, req, res, next) => {
-	if(err && err.name == 'UnauthorizedError') {
-		res.status(401).send('Invalid Token');
-	}
+    if (err && err.name == 'UnauthorizedError') {
+        res.status(401).send('Invalid Token')
+    }
 })
 
 // connect to temporary mongodb ran on local machine
-mongoose.connect(DB_ADDR);
-if (!isProduction) mongoose.set("debug", true);
+mongoose.connect(DB_ADDR)
+if (!isProduction) mongoose.set('debug', true)
 
 // Models and Routers
-require("./models/users");
-require("./config/passport");
-app.use(require("./routes"));
+require('./models/users')
+require('./config/passport')
+app.use(require('./routes'))
 
-module.exports = app;
+module.exports = app
