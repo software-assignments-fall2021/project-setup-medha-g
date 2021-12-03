edaf06d4312217f9f84d20a27697b843655d21d4
Morgan Xu <morganxu2004@outlook.com>
2021-12-02 21:47:34 -0500

    Improved style of the add subscription page

--- front-end/subtrackt/src/components/Subscription.jsx
+++ front-end/subtrackt/src/components/Subscription.jsx
@@ -21,7 +21,7 @@ function Subscription(props) {
 			</div>
 			<div className="col-md-3 col-sm-6 col-xs-12 bootCols">
 				<h4>Plan Details</h4>
-				<p>Price: {props.plan.price} for {props.plan.time_quantity} {props.plan.time_unit}</p>
+				<p>Price: ${props.plan.price} for {props.plan.time_quantity} {props.plan.time_unit}</p>
 			</div>
 			<div className="col-md-3 col-sm-6 col-xs-12 bootCols">
 				<img src  = {deleteImage} onClick={handleClick} alt="Delete" className="custom-delete" height="30px" width="30px"/>
