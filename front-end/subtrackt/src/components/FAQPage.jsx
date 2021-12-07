5d4f25b317e7651508d40c434ab28de4b027fc4b
Morgan <hx801@nyu.edu>
2021-12-07 00:31:26 -0500

    Fixed some styling issue with pages

--- front-end/subtrackt/src/components/FAQPage.jsx
+++ front-end/subtrackt/src/components/FAQPage.jsx
@@ -1,51 +1,46 @@
 import React from "react";
+import Footer from "./Footer";
 
 const FAQPage = (props) => {
   return (
     <div className="faqpage">
-      <div className="column">
-        <div className="row">
-          <div className="card">
-            <div className="container">
-              <h1>Frequently Asked Questions</h1>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="card">
-            <div className="container">
-              <h2>How does Subtrackt help its users?</h2>
-              <p>
-                Some text that answers the question lorem ipsum ipsum lorem.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="card">
-            <div className="container">
-              <h2>
-                How can Subtrackt help me realize subscriptions I forgot about?
-              </h2>
-              <p>
-                Some text that answers the question lorem ipsum ipsum lorem.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="card">
-            <div className="container">
-              <h2>Zachary Waxman</h2>
-              <p>
-                Some text that answers the question lorem ipsum ipsum lorem.
-              </p>
-            </div>
+      <div className="row">
+        <div className="card">
+          <div className="container">
+            <h1>Frequently Asked Questions</h1>
           </div>
         </div>
       </div>
+
+      <div className="row">
+        <div className="card">
+          <div className="container">
+            <h2>How does Subtrackt help its users?</h2>
+            <p>Subtrack will keep trac of your subscription so you no longer wonder who is taking 5 dollars out of your bank account every month. Our app let you add, delete, and track subscription easily and provide you with break down of your information to help you make better decision.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="card">
+          <div className="container">
+            <h2>
+              How can Subtrackt help me realize subscriptions I forgot about?
+            </h2>
+            <p>We uses Plaid to parse your transaction history in order to recognize transactions made to subscription companies. With just one click of your button, forget going through your numerous transactions to find the subscription you forgot about. Don't Worry! We will never save your transaction data or reveal them to anyone else. All your transaction are deleted after parsing.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="card">
+          <div className="container">
+            <h2>Zachary Waxman</h2>
+            <p>Some text that answers the question lorem ipsum ipsum lorem.</p>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
