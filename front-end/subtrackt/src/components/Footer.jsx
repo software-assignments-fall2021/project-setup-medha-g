83afbafea3170cbe80bd86ebc0e2450e3ce6a878
Medha Gupta <medha4@10-21-130-174.dynapool.wireless.nyu.edu>
2021-12-08 20:54:06 -0500

    added social media links

--- front-end/subtrackt/src/components/Footer.jsx
+++ front-end/subtrackt/src/components/Footer.jsx
@@ -27,22 +27,22 @@ const Footer = () => {
                             <p>SubTrackt is a Web Application that allows users to keep track of their subscriptions. Subscriptions are either populated via credit card transaction history or via manual entering. Users also have the opportunity to cancel and add subscriptions that they are interested in through SubTrackt and they can evaluate exactly where each of their money is going.</p>
                         </div>
                         <div className="col item social">
-                            <a href="/">
+                            <a href="https://www.facebook.com/" target="_blank">
                                 <i className="icon ion-social-facebook">
                                     <img src={facebook} height="20px" alt="" />
                                 </i>
                             </a>
-                            <a href="/">
+                            <a href="https://twitter.com/home" target="_blank">
                                 <i className="icon ion-social-twitter">
                                     <img src={twitter} height="20px" alt="" />
                                 </i>
                             </a>
-                            <a href="/">
+                            <a href="https://www.linkedin.com/feed/" target="_blank">
                                 <i className="icon ion-social-snapchat">
                                     <img src={linkedin} height="20px" alt="" />
                                 </i>
                             </a>
-                            <a href="/">
+                            <a href="https://www.instagram.com/" target="_blank">
                                 <i className="icon ion-social-instagram">
                                     <img src={instagram} height="20px" alt="" />
                                 </i>
