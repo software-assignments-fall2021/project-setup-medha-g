f961eec801c820d3634cffa28e8c1a68acb87b9f
Medha Gupta <medha4@10-21-130-174.dynapool.wireless.nyu.edu>
2021-12-08 20:50:35 -0500

    added animation to faq page

--- front-end/subtrackt/src/components/FAQPage.jsx
+++ front-end/subtrackt/src/components/FAQPage.jsx
@@ -1,45 +1,29 @@
 import React from "react";
 import Footer from "./Footer";
+import '../styles/FAQPage.css'; 
 
 const FAQPage = (props) => {
   return (
     <div className="faqpage">
-      <div className="row">
-        <div className="card">
-          <div className="container">
-            <h1>Frequently Asked Questions</h1>
-          </div>
+      <br/><br/><br/><h1>Frequently Asked Questions</h1><br/><br/>
+      <details open>
+        <summary>How does SubTrackt help its users?</summary>
+        <div class="faq__content">
+          <p>Subtrack will keep track of your subscription so you no longer wonder who is taking 5 dollars out of your bank account every month. Our app let you add, delete, and track subscription easily and provide you with break down of your information to help you make better decision.</p>
         </div>
-      </div>
-
-      <div className="row">
-        <div className="card">
-          <div className="container">
-            <h2>How does Subtrackt help its users?</h2>
-            <p>Subtrack will keep trac of your subscription so you no longer wonder who is taking 5 dollars out of your bank account every month. Our app let you add, delete, and track subscription easily and provide you with break down of your information to help you make better decision.</p>
-          </div>
+      </details>
+      <details>
+        <summary>How can SubTrackt help me realize subscriptions I forgot about?</summary>
+        <div class="faq__content">
+          <p>We use Plaid to parse your transaction history in order to recognize transactions made to subscription companies. With just one click of your button, forget going through your numerous transactions to find the subscription you forgot about. Don't Worry! We will never save your transaction data or reveal them to anyone else. All your transaction are deleted after parsing.</p>
         </div>
-      </div>
-
-      <div className="row">
-        <div className="card">
-          <div className="container">
-            <h2>
-              How can Subtrackt help me realize subscriptions I forgot about?
-            </h2>
-            <p>We uses Plaid to parse your transaction history in order to recognize transactions made to subscription companies. With just one click of your button, forget going through your numerous transactions to find the subscription you forgot about. Don't Worry! We will never save your transaction data or reveal them to anyone else. All your transaction are deleted after parsing.</p>
-          </div>
+      </details>
+      <details>
+        <summary>Why was SubTrackt created?</summary>
+        <div class="faq__content">
+          <p>Our founder had trouble keeping track of his subscriptions and realized he had unnecessary bills on his credit card statements. And so, he decided to gather a team to create a solution.</p>
         </div>
-      </div>
-
-      <div className="row">
-        <div className="card">
-          <div className="container">
-            <h2>Zachary Waxman</h2>
-            <p>Some text that answers the question lorem ipsum ipsum lorem.</p>
-          </div>
-        </div>
-      </div>
+      </details><br/><br/>
       <Footer />
     </div>
   );
